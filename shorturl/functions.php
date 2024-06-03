b167fb608136bef7c90051396b6a64f4a7961131
airman66 <n89509176381@gmail.com>
2024-06-03 23:56:29 +0300

    remove repeats

--- shorturl/functions.php
+++
@@ -1,7 +0,0 @@
-<?php
-function sendResponse($status = 200, $body = '', $content_type = 'application/json') {
-    header('HTTP/1.1 ' . $status);
-    header('Content-Type: ' . $content_type. "; charset=utf-8");
-    echo json_encode($body);
-    exit();
-}
